96213c5a0ddf0e9db0213c69ccce0e97cad5f1e8
TeenSpirit1408 <Mentalist26021999@gmail.com>
2016-09-26 21:57:02 +0300

    Tasks 30

--- task_30.py
+++ task_30.py
@@ -7,83 +7,52 @@ from pyrob.api import *
 def task_9_3():
     k = 0
     a = 1
+    b = 0
     while not wall_is_on_the_right():
         k += 1
         move_right()
     while not wall_is_on_the_left():
         move_left()
-    for i in range(k-a):
+
+
+    while b != (k//2):
         move_right()
-        fill_cell()
-    a += 2
+        for i in range(k - a):
+            fill_cell()
+            move_right()
+
+
+        move_down()
+        for i in range(k - a):
+            fill_cell()
+            move_down()
+
+
+        move_left()
+        for i in range(k - a):
+            fill_cell()
+            move_left()
+
+
+        move_up()
+        for i in range(k - a):
+            fill_cell()
+            move_up()
+        b += 1
+        move_down()
+        move_right()
+        a += 2
+
+
     while not wall_is_on_the_left():
         move_left()
-    move_down()
-    move_right(a-2)
-    for i in range(k-a):
-        move_right()
-        fill_cell()
-    while not wall_is_above():
-        move_up()
-    while not wall_is_on_the_right():
-        move_right()
-
-
-
-    a = 1
-    for i in range(k-a):
-        move_down()
-        fill_cell()
-    a += 2
-    while not wall_is_above():
-        move_up()
-    move_left()
-    move_down(a-2)
-    for i in range(k-a):
-        move_down()
-        fill_cell()
     while not wall_is_beneath():
         move_down()
-    while not wall_is_on_the_right():
-        move_right()
 
 
 
-    a = 1
-    for i in range(k-a):
-        move_left()
-        fill_cell()
-    a += 2
-    while not wall_is_on_the_right():
-        move_right()
-    move_up()
-    move_left(a-2)
-    for i in range(k-a):
-        move_left()
-        fill_cell()
-    while not wall_is_beneath():
-        move_down()
-    while not wall_is_on_the_left():
-        move_left()
 
 
-
-    a = 1
-    for i in range(k-a):
-        move_up()
-        fill_cell()
-    a += 2
-    while not wall_is_beneath():
-        move_down()
-    move_right()
-    move_up(a-2)
-    for i in range(k-a):
-        move_up()
-        fill_cell()
-    while not wall_is_beneath():
-        move_down()
-    while not wall_is_on_the_left():
-        move_left()
 
 
 

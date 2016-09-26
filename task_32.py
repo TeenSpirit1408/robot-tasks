3fe90f4f57c8ede72914950b710e1ad57c562546
TeenSpirit1408 <Mentalist26021999@gmail.com>
2016-09-26 23:19:08 +0300

    Tasks 32

--- task_32.py
+++ task_32.py
@@ -6,6 +6,7 @@ from pyrob.api import *
 @task(delay=0.01)
 def task_8_18():
     ax = 0
+    k = 0
     if wall_is_beneath() and wall_is_above():
         fill_cell()
     while not wall_is_on_the_right():
@@ -16,13 +17,13 @@ def task_8_18():
             while not wall_is_above():
                 move_up()
                 if cell_is_filled():
-                    ax += 1
+                    k += 1
+                    mov("ax", k)
                 else:
                     fill_cell()
             while not wall_is_beneath():
                 move_down()
         if not wall_is_above() and not wall_is_beneath():
             break
-    print(ax)
 if __name__ == '__main__':
     run_tasks()
